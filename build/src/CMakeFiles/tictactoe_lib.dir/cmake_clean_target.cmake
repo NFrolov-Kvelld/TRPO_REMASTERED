cc51b9b672d226fed066dc9b58e376434c41b479
Nikita_Frolov <nikitafrolov2004007@gmail.com>
2025-04-24 22:13:18 +0700

    final v

--- build/src/CMakeFiles/tictactoe_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtictactoe_lib.a"
-)
--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kvelld/rem/tests
-# Build directory: /home/kvelld/rem/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/kvelld/rem/build/tests/tictactoe_tests[1]_include.cmake")
